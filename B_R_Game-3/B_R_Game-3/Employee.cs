f4322b1f3a8c95e1d33d60c0b57d031af44f0604
BaGarus <100210809+BaGarus@users.noreply.github.com>
2023-05-23 16:32:48 +0300

    Add files via upload

---
+++ B_R_Game-3/B_R_Game-3/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_R_Game_3
+{
+    internal class Employee
+    {
+        private string _FIO;
+        private string _Gender;
+        private double _Salary;
+        private double _Id;
+        private double _Departament;
+        private string _Description;
+        private double count;
+
+        public Employee(string _fio, string gender, double salary, double departament, string description)
+        {
+            _FIO = fio;
+            _Id = count++;
+            _Gender = gender;
+            _Salary = salary;
+            _Description = description;
+        }
+
+        public string fio
+        {
+            get { return _FIO; }
+            set { _FIO = value; }
+        }
+
+        public string Gender
+        {
+            get => _Gender;
+            set => _Gender = value;
+        }
+
+        public double Salary
+        {
+            get => _Salary;
+            set => _Salary = value;
+        }
+
+        public double Departament
+        {
+            get => _Departament;
+            set => _Departament = value;
+        }
+
+        public string Description
+        {
+            get => _Description;
+            set => _Description = value;
+        }
+
+    }
+}
---
+++ B_R_Game-3/B_R_Game-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using B_R_Game_3;
+
+ArrayList List = new ArrayList();
+
+
+List<Employee> emplist = new List<Employee>()
+{
+    new Employee("Marina", "woman", 4500, 1, "yes"),
+    new Employee("Mark", "men", 5500, 2, "yes"),
+    new Employee("Markis", "men", 7500, 3, "yes"),
+    new Employee("Danila", "men", 3500, 4, "no"),
+    new Employee("Happy", "men", 5500, 5, "yes"),
+    new Employee("Sarina", "woman", 5700, 6, "yes"),
+    new Employee("Nastya", "woman", 4350, 7, "yes"),
+    new Employee("Sasha", "woman", 5800, 8, "yes"),
+    new Employee("Adolf", "men", 9500, 9, "Too good"),
+    new Employee("Dream", "men", 8200, 10, "no"),
+};
+
+void methodall()
+{
+    foreach (Employee emp in emplist)
+    {
+        Console.WriteLine(emp.fio + "");
+        Console.WriteLine(emp.Gender + "");
+        Console.WriteLine(emp.Salary + "\n");
+    }
+}
+methodall();
+Console.WriteLine("Hi bro nice Ass");
+
